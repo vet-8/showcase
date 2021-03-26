9e8017e0c3cf8600712c46de386aa80f2f52c04d
Ivan Adrianto <ivanadrianto64@gmail.com>
2021-03-26 14:27:57 +0700

    adding app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import Home from "./pages/Home";
+import Home from "./pages/public-all/home/Home";
 import { Switch, Route, withRouter, HashRouter } from "react-router-dom";
-import SearchClinic from "./pages/SearchClinic";
-import Register from "./components/Register";
-import BookingDetails from "./pages/BookingDetails";
-import BookingResume from "./pages/BookingResume";
-import Dokter from "./pages/Dokter";
-import User from "./pages/EditUser";
-import KlinikAdmin from "./pages/EditClinic";
-import Chat from "./pages/chat/components/Chat/Chat";
-import Join from "./pages/chat/components/Join/Join";
-import Login from "./components/Login";
-import Role from "./components/Role";
-import UserHistory from "./pages/UserHistory"; //
-import EditClinic from "./pages/EditClinic";
-import EditDoctor from "./pages/EditDoctor";
-import EditUser from "./pages/EditUser";
-import ManageDoctor from "./pages/ManageDoctor";
-import ForUserEdit from "./pages/ForUserEdit";
-// import ForUserHistory from './pages/ForUserHistory'
-import EditProfileClinic from './pages/EditProfileClinic'
-import ChatDashboard from './pages/chat-firebase/Dashboard/dashboard'
-import ChatDashboardNew from './pages/chat-firebase/Dashboard/dashboardNew'
+import SearchClinic from "./pages/public-all/search/SearchClinic";
+import Register from "./pages/public-all/login-reigster/Register";
+import BookingDetails from "./pages/patient/booking/BookingDetails";
+import BookingResume from "./pages/patient/booking-resume/BookingResume";
+import Dokter from "./pages/veterinarian/Dokter";
+import Login from "./pages/public-all/login-reigster/Login";
+import Role from "./pages/public-all/login-reigster/Role";
+import UserHistory from "./pages/patient/user-profile/UserHistory";
+import EditDoctor from "./pages/veterinarian/edit-profil/EditDoctor";
+import ManageDoctor from "./pages/admin/manage-doctor/ManageDoctor";
+import ForUserEdit from "./pages/patient/user-profile/edit-profil/ForUserEdit";
+import EditProfileClinic from './pages/admin/edit-profil/EditProfileClinic'
+import ChatDashboard from './pages/public-all/chat-firebase/Dashboard/dashboard'
+import ChatDashboardNew from './pages/public-all/chat-firebase/Dashboard/dashboardNew'
 
 
 function App() {
@@ -30,10 +23,6 @@ function App() {
         <Switch>
           <Route path="/dokter">
             <Dokter />
-          </Route>
-
-          <Route path="/user">
-            <User />
           </Route>
           <Route path="/login">
             <Login />
@@ -47,29 +36,15 @@ function App() {
           <Route exact path="/book-resume">
             <BookingResume />
           </Route>
-          <Route path="/klinik-admin">
-            <KlinikAdmin />
-          </Route>
-          <Route path="/edituser">
-            <EditUser />
-          </Route>
           <Route path="/editdoctor">
             <EditDoctor />
           </Route>
-          <Route path="/editclinic">
-            <EditClinic />
-          </Route>
-          {/* <Route path="/userhistory">
-            <UserHistory />
-          </Route> */}
           <Route path="/managedoctor">
             <ManageDoctor />
           </Route>
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/chat-start" exact component={Join} />
-          <Route path="/chat" component={Chat} />
           <Route exact path="/register/input">
             <Register />
           </Route>
@@ -98,83 +73,3 @@ function App() {
 }
 
 export default withRouter(App);
-
-// // versi mas Frey
-// import Home from "./pages/Home";
-// import Dokter from "./pages/Dokter";
-// import SearchClinic from "./pages/SearchClinic";
-// import Register from "./components/Register";
-// import Login from "./components/Login";
-// import Role from "./components/Role";
-// import { Switch, Route, withRouter } from "react-router-dom";
-
-// // import BookingDetails from "./pages/BookingDetails";
-// // import BookingResume from "./pages/BookingResume";
-// import User from "./pages/EditUser";
-// import KlinikAdmin from "./pages/EditClinic";
-// // import Chat from './pages/chat/components/Chat/Chat';
-// // import Join from './pages/chat/components/Join/Join';
-
-// import UserHistory from "./pages/UserHistory";
-// import EditClinic from "./pages/EditClinic";
-// import EditDoctor from "./pages/EditDoctor";
-// import EditUser from "./pages/EditUser";
-// import ManageDoctor from "./pages/ManageDoctor";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Switch>
-//         <Route path="/user">
-//           <User />
-//         </Route>
-//         {/* <Route exact path="/book-clinic/:id">
-//           <BookingDetails />
-//         </Route>
-//         <Route exact path="/book-resume">
-//           <BookingResume />
-//         </Route> */}
-//         <Route path="/klinik-admin">
-//           <KlinikAdmin />
-//         </Route>
-//         <Route path="/edituser">
-//           <EditUser />
-//         </Route>
-//         <Route path="/editdoctor">
-//           <EditDoctor />
-//         </Route>
-//         <Route path="/editclinic">
-//           <EditClinic />
-//         </Route>
-//         <Route path="/userhistory">
-//           <UserHistory />
-//         </Route>
-//         <Route path="/managedoctor">
-//           <ManageDoctor />
-//         </Route>
-//         {/* <Route path="/chat-start" exact component={Join} /> */}
-//         {/* <Route path="/chat" component={Chat} /> */}
-//         <Route exact path="/login">
-//           <Login />
-//         </Route>
-//         <Route exact path="/register/input">
-//           <Register />
-//         </Route>
-//         <Route exact path="/register">
-//           <Role />
-//         </Route>
-//         <Route path="/dokter">
-//           <Dokter />
-//         </Route>
-//         <Route path="/klinik">
-//           <SearchClinic />
-//         </Route>
-//         <Route exact path="/">
-//           <Home />
-//         </Route>
-//       </Switch>
-//     </div>
-//   );
-// }
-
-// export default withRouter(App);
